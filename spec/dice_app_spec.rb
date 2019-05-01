9eee309de6bfbc8b539c72669c14d6a8cf6e0ddc
Francesca Chater <francescachater@gmail.com>
2019-05-01 13:04:02 +0100

    Test roll method to return number between 1 and 6

--- spec/dice_app_spec.rb
+++ spec/dice_app_spec.rb
@@ -1,2 +1,17 @@
+require './lib/dice_app'
+
 describe Dice do
+#  it 'should have a roll method' do
+#    dice = Dice.new
+#    expect(dice).to respond_to(:roll)
+#
+describe '#roll' do
+  it 'should give a number between 1 and 6' do
+    dice = Dice.new
+    result = dice.roll
+    expect(
+      result >= 1 && result <= 6
+      ) .to eq(true)
+    end
+  end
 end
